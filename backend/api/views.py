46a9b3ea11eb9038931b5d4f0ac401d39d5df37d
Andrey Kruglov <altos047@gmail.com>
2022-11-02 07:28:37 +0600

    shoplist8.1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,20 @@
-from datetime import datetime
+#from datetime import datetime
 
-from django.db.models import Sum
-from django.http import HttpResponse
+#from django.db.models import Sum
+#from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
+#from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from recipes.models import (
     Favourite,
     Ingredient,
-    IngredientInRecipe,
+    #IngredientInRecipe,
     Recipe,
     ShoppingCart,
     Tag
