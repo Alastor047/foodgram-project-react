3e26606c505de844c41c99bd727ec2d044a0c72b
Andrey Kruglov <altos047@gmail.com>
2022-11-02 06:35:16 +0600

    shoplist7

--- backend/api/services.py
+++ backend/api/services.py
@@ -1,26 +1,26 @@
-from datetime import datetime
-
-from django.db.models import Sum
-from recipes.models import IngredientInRecipe
-
-
-def set_shopping_list(self, request, user):
-    ingredients = IngredientInRecipe.objects.filter(
-        recipe__shopping_cart__user=user
-    ).values(
-        'ingredient__name',
-        'ingredient__measurement_unit'
-    ).annotate(amount=Sum('amount'))
-    today = datetime.today()
-    shopping_list = (
-        f'Список покупок для: {user}\n\n'
-        f'Дата: {today():%Y-%m-%d}\n\n'
-    )
-    shopping_list += '\n'.join([
-        f'- {ingredient["ingredient__name"]} '
-        f'({ingredient["ingredient__measurement_unit"]})'
-        f' - {ingredient["amount"]}'
-        for ingredient in ingredients
-    ])
-    shopping_list += f'\n\nFoodgram ({today():%Y})'
-    return shopping_list
+#from datetime import datetime
+#
+#from django.db.models import Sum
+#from recipes.models import IngredientInRecipe
+#
+#
+#def set_shopping_list(self, request, user):
+#    ingredients = IngredientInRecipe.objects.filter(
+#        recipe__shopping_cart__user=user
+#    ).values(
+#        'ingredient__name',
+#        'ingredient__measurement_unit'
+#    ).annotate(amount=Sum('amount'))
+#    today = datetime.today()
+#    shopping_list = (
+#        f'Список покупок для: {user}\n\n'
+#        f'Дата: {today():%Y-%m-%d}\n\n'
+#    )
+#    shopping_list += '\n'.join([
+#        f'- {ingredient["ingredient__name"]} '
+#        f'({ingredient["ingredient__measurement_unit"]})'
+#        f' - {ingredient["amount"]}'
+#        for ingredient in ingredients
+#    ])
+#    shopping_list += f'\n\nFoodgram ({today():%Y})'
+#    return shopping_list
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
-#from django.db.models import Sum
+from datetime import datetime
+
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
@@ -12,7 +14,7 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from recipes.models import (
     Favourite,
     Ingredient,
-    #IngredientInRecipe,
+    IngredientInRecipe,
     Recipe,
     ShoppingCart,
     Tag
@@ -20,7 +22,7 @@ from recipes.models import (
 from api.filters import IngredientFilter, RecipeFilter
 from api.pagination import CustomPagination
 from api.permissions import IsAdminAuthorOrReadOnly, IsAdminOrReadOnly
-from api.services import set_shopping_list
+#from api.services import set_shopping_list
 from api.serializers import (
     RecipeShortSerializer,
     IngredientSerializer,
@@ -100,19 +102,33 @@ class RecipeViewSet(ModelViewSet):
 
     @action(
         detail=False,
-        #permission_classes=[IsAuthenticated]
+        permission_classes=[IsAuthenticated]
     )
     def download_shopping_cart(self, request):
         user = request.user
         if not user.shopping_cart.exists():
             return Response(status=HTTP_400_BAD_REQUEST)
-        #ingredients = IngredientInRecipe.objects.filter(
-        #    recipe__shopping_cart__user=request.user
-        #).values(
-        #    'ingredient__name',
-        #    'ingredient__measurement_unit'
-        #).annotate(amount=Sum('amount'))
-        shopping_list = set_shopping_list(user, request)
+
+        ingredients = IngredientInRecipe.objects.filter(
+            recipe__shopping_cart__user=request.user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).annotate(amount=Sum('amount'))
+
+        today = datetime.today()
+        shopping_list = (
+            f'Список покупок для: {user.get_full_name()}\n\n'
+            f'Дата: {today:%Y-%m-%d}\n\n'
+        )
+        shopping_list += '\n'.join([
+            f'- {ingredient["ingredient__name"]} '
+            f'({ingredient["ingredient__measurement_unit"]})'
+            f' - {ingredient["amount"]}'
+            for ingredient in ingredients
+        ])
+        shopping_list += f'\n\nFoodgram ({today:%Y})'
+
         filename = f'{user.username}_shopping_list.txt'
         response = HttpResponse(shopping_list, content_type='text/plain')
         response['Content-Disposition'] = f'attachment; filename={filename}'
